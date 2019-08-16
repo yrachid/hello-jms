4af9f6e0712aaa503f57e899339f8dfbe47d5995
yrachid <anuar.yasser@gmail.com>
2019-08-16 17:26:02 -0300

    BaseFunctionalTest

---
+++ src/test/java/functional/BaseFunctionalTest.java
@@ -0,0 +1,38 @@
+package functional;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = TweetPublishingTest.TestConfig.class)
+public class BaseFunctionalTest {
+
+    @LocalServerPort
+    protected int port;
+
+    @Configuration
+    @EnableAutoConfiguration
+    @ComponentScan("com.example.hello.jms")
+    public static class TestConfig {
+    }
+
+    protected RequestSpecification theContextToCommunicateToTheApplication() {
+
+        return new RequestSpecBuilder()
+            .setPort(port)
+            .setAccept(ContentType.JSON)
+            .build();
+
+    }
+
+}
--- src/test/java/functional/HealthCheckTest.java
+++ src/test/java/functional/HealthCheckTest.java
@@ -1,43 +1,15 @@
 package functional;
 
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT, classes = HealthCheckTest.TestConfig.class)
-public class HealthCheckTest {
-
-    @Configuration
-    @EnableAutoConfiguration
-    @ComponentScan("com.example.hello.jms")
-    public static class TestConfig {
-    }
-
-    @LocalServerPort
-    private int port;
+public class HealthCheckTest extends BaseFunctionalTest {
 
     @Test
     public void responds_with_plain_text_ok_message() {
-        RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setPort(port)
-            .setAccept(ContentType.JSON)
-            .build();
-
-        RestAssured
-            .given(requestSpec)
+        given(theContextToCommunicateToTheApplication())
             .when()
             .get("/health")
             .then()
