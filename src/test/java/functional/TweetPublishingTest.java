45a5e32d439271e0d6f10a9bf94cbc720b261230
yrachid <anuar.yasser@gmail.com>
2019-08-16 18:30:09 -0300

    More tasks

--- src/test/java/functional/TweetPublishingTest.java
+++ src/test/java/functional/TweetPublishingTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class TweetPublishingTest extends BaseFunctionalTest {
 
     @Test
-    public void it_runs() {
+    public void pushes_a_tweet_to_the_queue_via_the_tweet_endpoint() {
         System.out.println("It does nothing besides proving that Spring test config works.");
     }
 }
